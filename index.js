3cce5372f5ea98d0c4fca28f6bcb8af697d7c413
npncodesmash <64137398+npncodesmash@users.noreply.github.com>
2020-05-26 09:41:41 +0700

    Function query mainshift
    getUserMainShift ดึงข้อมูลuser ใน กะ
    getMainShift ดึงข้อมูล กะ

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function insert(sql)//name,age,permission)
 }
 
 async function select(sql, res) {
-	console.log("SQL : " + sql);
+	// console.log("SQL : " + sql);
 	if (isConnected) {
 
 		database.query(sql, (error, result) => {
@@ -78,7 +78,7 @@ app.get('/api/users', cors(), bodyParser, async (req, res) => {
 
 	sql = "SELECT * FROM `user`";
 	if (userid != null) {
-		sql += " WHERE `id` = '" + userid + "'";
+		sql += " WHERE `user_id` = '" + userid + "'";
 	}
 	select(sql, res);
 
@@ -130,7 +130,7 @@ app.get('/api/shifts', cors(), bodyParser, async (req, res) => {
 app.get('/api/getShift', cors(), bodyParser, async (req, res) => {
 
 	user_id = req.query.id;
-	sql = "SELECT * FROM shift INNER JOIN user ON shift_user_id=user.user_id WHERE shift_user_id =" + user_id;
+	sql = "SELECT * FROM shift INNER JOIN user ON shift_user_id=user.user_id INNER JOIN main_shift ON shift_main_id=main_shift.main_shift_id WHERE shift_user_id =" + user_id;
 	const result = select(sql, res);
 
 });
@@ -143,6 +143,106 @@ app.get('/api/userInShift', cors(), bodyParser, async (req, res) => {
 	const result = select(sql, res);
 });
 
+app.get('/api/getMainShift', cors(), bodyParser, (req, res) => {
+	shift_date = req.query.date;
+	shift_main_id = req.query.shift_main_id;
+	getMainShift(shift_date, shift_main_id, res);
+});
+
+
+app.get('/api/userInMainShift', cors(), bodyParser, (req, res) => {
+
+	main_shift_id = req.query.main_shift_id;
+	getUserMainShift(main_shift_id, res);
+});
+
+
+async function getUserMainShift(main_shift_id, res) {
+	sql = "SELECT * FROM shift WHERE `shift_main_id` = '" + main_shift_id + "'";
+	const respones = [];
+	if (isConnected) {
+		console.log("SQL :" + sql)
+		database.query(sql, (error, result) => {
+			if (error) {
+				throw error;
+			} else {
+				res.send(result);
+
+			}
+		});
+
+
+	}
+	else {
+		console.log("Database is not connect");
+	}
+}
+// var async = require('async');
+async function getMainShift(shift_date, shift_main_id, res) {
+	sql = "SELECT * FROM main_shift WHERE `main_shift_id` = '" + shift_main_id + "'";
+	const respones = [];
+	if (isConnected) {
+		console.log("SQL :" + sql)
+		database.query(sql, (error, result) => {
+			if (error) {
+				throw error;
+			} else {
+
+				res.send(result);
+				// mainShiftRows = JSON.parse(JSON.stringify(result))
+				// mainShiftRows.forEach(element => {
+				// 	console.log("---------------------loop-----------------------")
+				// 	sql_loop = "SELECT * FROM shift WHERE shift_main_id=" + element.main_shift_id + " AND shift_user_id !=" + user_id;
+				// 	console.log("SQL : " + sql_loop)
+				// 	database.query(sql_loop, (error2, result2) => {
+				// 		if (error2) {
+				// 			throw error2;
+				// 		} else {
+				// 			console.log("---------------------shiftRows---------------------");
+				// 			shiftRows = JSON.parse(JSON.stringify(result2))
+				// 			console.log(shiftRows)
+				// 			// return shiftRows;
+				// 		}
+				// 	});
+
+				// });
+				// console.log("---------------------respones---------------------");
+				// console.log(respones);
+			}
+		});
+
+		// let resultRows = Array();
+		// const result = database.query(sql, async function (err, rows, fields) {
+		// 	console.log("---------------------result---------------------")
+		// 	// mainShiftRows = JSON.parse(JSON.stringify(result))
+		// 	console.log(rows)
+
+		// 	async.each(rows, function (row, callback) {
+		// 		sql_loop = "SELECT * FROM shift WHERE shift_main_id=" + row.main_shift_id + " AND shift_user_id !=" + user_id;
+		// 		database.query(sql_loop, function (err, innerRow) {
+		// 			resultRows.push(innerRow);
+		// 			callback(null);
+		// 		});
+		// 	}, async function () {
+		// 		//This is the final function which get executed when loop is done.
+		// 		const response = {
+		// 			statusCode: 200,
+		// 			headers: {
+		// 				"Access-Control-Allow-Origin": "*" // Required for CORS support to work
+		// 			},
+		// 			body: JSON.stringify({
+		// 				success: true,
+		// 				data: resultRows
+		// 			})
+		// 		};
+		// 		callback(null, response);
+		// 	});
+		// });
+	}
+	else {
+		console.log("Database is not connect");
+	}
+}
 
 
 
